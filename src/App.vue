8d4438ac1613ac470e6b156cd3e06b09c66e9794
BASARA0826 <clearheart260@gmail.com>
2024-12-06 03:34:41 +0900

    サイドバーの作成

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,44 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <router-view />
+    <v-navigation-drawer v-model="drawer" app>
+      <v-sheet color="grey lighten-4" class="pa-4">
+        <v-avatar class="mb-4" color="grey darken-1" size="64"></v-avatar>
+
+        <div class="username">john@vuetifyjs.com</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item v-for="[icon, text] in links" :key="icon" link>
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null,
+    links: [
+      ["mdi-home", "タスク一覧"],
+      ["mdi-list-box-outline", "完了タスク一覧"],
+      ["mdi-pencil-outline", "タスク作成"],
+      ["mdi-account-edit-outline", "アカウント設定"],
+      ["mdi-logout", "ログアウト"],
+    ],
+  }),
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -28,5 +60,18 @@ nav {
       color: #42b983;
     }
   }
+}
+
+.grey.darken-1 {
+  background-color: #757575 !important;
+  border-color: #757575 !important;
+}
+
+.pa-4 {
+  padding: 16px !important;
+}
+
+.username {
+  padding-top: 10px;
 }
 </style>
