3a10e62e84dd07f9298ff5373cc107b77855fd83
BASARA0826 <clearheart260@gmail.com>
2024-12-09 09:54:47 +0900

    TaskCreateView.vueの作成

---
+++ src/views/TaskCreateView.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-app id="task-create">
+    <v-app-bar app color="primary" dark>
+      <!-- 戻るボタン -->
+      <v-btn icon @click="$router.go(-1)">
+        <v-icon>mdi-arrow-left</v-icon>
+      </v-btn>
+      <v-toolbar-title>タスク作成</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-card class="pa-4" outlined>
+          <!-- タイトル入力 -->
+          <v-card-title>
+            <v-text-field
+              label="タスクのタイトル"
+              v-model="title"
+              outlined
+              dense
+            ></v-text-field>
+          </v-card-title>
+
+          <!-- 内容入力 -->
+          <v-card-text>
+            <v-textarea
+              label="タスクの内容"
+              v-model="content"
+              outlined
+              dense
+            ></v-textarea>
+          </v-card-text>
+
+          <!-- 期限入力 -->
+          <v-card-subtitle>
+            <v-menu
+              ref="menu"
+              v-model="menu"
+              :close-on-content-click="false"
+              :nudge-right="40"
+              transition="scale-transition"
+              offset-y
+              min-width="290px"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field
+                  v-model="deadline"
+                  label="期限"
+                  readonly
+                  v-bind="attrs"
+                  v-on="on"
+                  outlined
+                  dense
+                ></v-text-field>
+              </template>
+              <v-date-picker
+                v-model="deadline"
+                @input="menu = false"
+              ></v-date-picker>
+            </v-menu>
+          </v-card-subtitle>
+
+          <!-- 作成ボタン -->
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" dark @click="createTask">
+              <v-icon left>mdi-plus</v-icon>
+              作成
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    title: "", // タスクのタイトル
+    content: "", // タスクの内容
+    deadline: "", // タスクの期限
+    menu: false, // 日付選択メニューの状態
+  }),
+  methods: {
+    createTask() {
+      // 作成ボタンの仮の挙動
+      alert(
+        "タスクが作成されました！\n" +
+          `タイトル: ${this.title}\n内容: ${this.content}\n期限: ${this.deadline}`
+      );
+    },
+  },
+};
+</script>
+
+<style></style>
