88c56b2e1d39b4d6eb181e81d86f41b857a0d8b6
BASARA0826 <clearheart260@gmail.com>
2024-12-10 10:48:44 +0900

    firebase.jsの作成

---
+++ src/firebase/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZEiK001yWsgHWTV-_TvbN-btsPsbzGgQ",
+  authDomain: "chronofade-4e095.firebaseapp.com",
+  projectId: "chronofade-4e095",
+  storageBucket: "chronofade-4e095.firebasestorage.app",
+  messagingSenderId: "998664682319",
+  appId: "1:998664682319:web:69676f550d39b804aaa33a",
+  measurementId: "G-MW3SDQVLBW",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
