163f861f66f3eeb08a9795fed3d5e56c0090d444
BASARA0826 <clearheart260@gmail.com>
2024-12-12 18:28:55 +0900

    Sidebarの表示ページをすみわけ

---
+++ src/components/layouts/Sidebar.vue
@@ -0,0 +1,79 @@
+<template>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-sheet color="grey lighten-4" class="pa-4">
+        <v-avatar class="mb-4" color="grey darken-1" size="64"></v-avatar>
+
+        <div class="username">HAL就職作品</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item
+          v-for="[icon, text, to] in links"
+          :key="icon"
+          :to="to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    drawer: null,
+    links: [
+      ["mdi-home", "タスク一覧", "/"],
+      ["mdi-list-box-outline", "完了タスク一覧", "/"],
+      ["mdi-pencil-outline", "タスク作成", "/create"],
+      ["mdi-account-edit-outline", "アカウント設定", "/config"],
+      ["mdi-logout", "ログアウト", "/"],
+    ],
+  }),
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+
+.grey.darken-1 {
+  background-color: #757575 !important;
+  border-color: #757575 !important;
+}
+
+.pa-4 {
+  padding: 16px !important;
+}
+
+.username {
+  padding-top: 10px;
+}
+</style>
