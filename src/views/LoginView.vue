444dbc7210c7f0eddcd9518e2f878d208142e0d1
BASARA0826 <clearheart260@gmail.com>
2025-01-19 18:57:40 +0900

    Sidebarにユーザー名を表示

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -89,6 +89,15 @@ export default {
         .then((result) => {
           console.log("success");
           console.log("user", result.user);
+
+          const auth = {
+            displayName: result.user.displayName,
+            email: result.user.email,
+            uid: result.user.uid,
+            refreshToken: result.user.refreshToken,
+          };
+          sessionStorage.setItem("user", JSON.stringify(auth));
+
           this.$router.push("task");
         })
         .catch((error) => {
