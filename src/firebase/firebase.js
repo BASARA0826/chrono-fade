bdb6008f8aae24c7236d7bac01fef5b19f83598e
BASARA0826 <144750622+BASARA0826@users.noreply.github.com>
2025-09-09 20:06:21 +0900

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,16 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZEiK001yWsgHWTV-_TvbN-btsPsbzGgQ",
-  authDomain: "chronofade-4e095.firebaseapp.com",
-  projectId: "chronofade-4e095",
-  storageBucket: "chronofade-4e095.firebasestorage.app",
-  messagingSenderId: "998664682319",
-  appId: "1:998664682319:web:69676f550d39b804aaa33a",
-  measurementId: "G-MW3SDQVLBW",
+  apiKey: "**********",
+  authDomain: "**********",
+  projectId: "**********",
+  storageBucket: "**********",
+  messagingSenderId: "**********",
+  appId: "**********",
+  measurementId: "**********",
 };
 
 firebase.initializeApp(firebaseConfig);
 
 export default firebase;
+
